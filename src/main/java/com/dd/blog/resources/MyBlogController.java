e2efb07253a1100a5c2663cfafb51cee5d14bc90
debiprasadp <debiprasadp@INHYDDEBIPRASAD.COMAKEIT.COM>
2019-05-02 18:21:07 +0530

    files added

---
+++ src/main/java/com/dd/blog/resources/MyBlogController.java
@@ -0,0 +1,38 @@
+package com.dd.blog.resources;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyBlogController {
+
+	@RequestMapping("/myblogindex")
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index");
+		return modelAndView;
+	}
+
+	@RequestMapping("/about")
+	public ModelAndView about() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("about");
+		return modelAndView;
+	}
+
+	@RequestMapping("/blogPost")
+	public ModelAndView blogPost() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("contact");
+		return modelAndView;
+	}
+
+	@RequestMapping("/contact")
+	public ModelAndView contact() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index");
+		return modelAndView;
+	}
+
+}
